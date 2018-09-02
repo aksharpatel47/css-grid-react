958c01cc8b4f2c7151a92529190554916315b725
Akshar Patel <akshar.patel.47@gmail.com>
2018-09-01 23:07:53 -0700

    feat: complete the spacing and placing exercise

--- pages/fundamentals.tsx
+++ pages/fundamentals.tsx
@@ -4,6 +4,7 @@ const container = css`
   display: grid;
   border: solid black;
   grid-template-columns: repeat(5, 1fr 2fr);
+  grid-template-rows: repeat(10, 50px);
   grid-gap: 20px;
   background-color: slateblue;
 `;
@@ -15,29 +16,58 @@ const item = css`
   background-color: yellow;
 `;
 
+const item1 = css`
+  grid-column: 3 / 5;
+`;
+
+const item2 = css`
+  grid-column: 5 / -1;
+`;
+
+const item5 = css`
+  grid-column: span 2;
+  grid-row: span 2;
+`;
+
+const item8 = css`
+  grid-row: span 2;
+`;
+
+const item15 = css`
+  grid-column: 1 / -1;
+`;
+
+const item18 = css`
+  grid-column: span 4 / 9;
+`;
+
+const item20 = css`
+  grid-row: 4 / span 3;
+`;
+
 function fundamentalsPage() {
   return (
     <div className={container}>
-      <div className={item}>1</div>
-      <div className={item}>2</div>
+      <div className={`${item} ${item1}`}>1</div>
+      <div className={`${item} ${item2}`}>2</div>
       <div className={item}>3</div>
       <div className={item}>4</div>
-      <div className={item}>5</div>
+      <div className={`${item} ${item5}`}>5</div>
       <div className={item}>6</div>
       <div className={item}>7</div>
-      <div className={item}>8</div>
+      <div className={`${item} ${item8}`}>8</div>
       <div className={item}>9</div>
       <div className={item}>10</div>
       <div className={item}>11</div>
       <div className={item}>12</div>
       <div className={item}>13</div>
       <div className={item}>14</div>
-      <div className={item}>15</div>
+      <div className={`${item} ${item15}`}>15</div>
       <div className={item}>16</div>
       <div className={item}>17</div>
-      <div className={item}>18</div>
+      <div className={`${item} ${item18}`}>18</div>
       <div className={item}>19</div>
-      <div className={item}>20</div>
+      <div className={`${item} ${item20}`}>20</div>
       <div className={item}>21</div>
       <div className={item}>22</div>
       <div className={item}>23</div>
